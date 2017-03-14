4a8316d05058e9ac406247dd6ec5a58ba74972b6
Tarique Syed <syed.tarique@gmail.com>
2017-03-14 08:38:22 -0400

    Adding The Intellij Project created for Application Management Center (iAMC)

---
+++ src/main/java/com/tarique/syed/amc/AppmgmtsystemApplication.java
@@ -0,0 +1,12 @@
+package com.tarique.syed.amc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmgmtsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmgmtsystemApplication.class, args);
+	}
+}
